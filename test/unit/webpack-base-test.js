d2690e888d5f2808a4c019543efd17e4e3b51bff
jiao_zhang <jiao_zhang@ctrip.com>
2021-08-01 23:23:59 +0800

    test: webpack.base test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -4,12 +4,16 @@ describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base');
   it('entry', () => {
     assert.equal(
-      baseConfig.entry.index,
-      '/Users/zhangjiao/前端学习/webpack/webpcak-learn/package/build-webpack/test/smoke/templete/src/index/index.js'
+      baseConfig.entry.index.indexOf(
+        'build-webpack/test/smoke/templete/src/index/index.js'
+      ) > -1,
+      true
     );
     assert.equal(
-      baseConfig.entry.search,
-      '/Users/zhangjiao/前端学习/webpack/webpcak-learn/package/build-webpack/test/smoke/templete/src/search/index.js'
+      baseConfig.entry.search.indexOf(
+        'build-webpack/test/smoke/templete/src/search/index.js'
+      ) > -1,
+      true
     );
   });
 });
